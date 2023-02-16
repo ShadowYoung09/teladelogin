97559439413d20a0ea47bebd22012b8d9789fb1b
Henrique Aleixo Mendes <89947763+ShadowYoung09@users.noreply.github.com>
2023-02-15 21:35:18 -0300

    Inital Commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+function Header(){
+    return(
+        <header>
+            <p>InShadow</p>
+            <div>    
+                <button>Entrar</button>
+                <button>Registre-se</button>
+            </div>
+        </header>
+    )
+}
+
+export default Header
